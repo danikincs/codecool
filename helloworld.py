663a643fe9d9421e6e1551804d4e3ebffdbb9806
Kincses Dani <danikincs@gmail.com>
2016-05-11 15:55:47 +0200

    Instructions added

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,10 @@
-
+#made by the website instructions
 import sys
 i="World"
+#if the program get nothing for the arg, it will change the name of the i variable to World-to print Hello World!
 
 for arg in sys.argv:
     if arg != ("helloworld.py"):
         i=arg
 
-print ("Hello",(i),"!")
+print ("Hello",(i),"!")    #if the script get a name it will print Hello (name(i variable)) !
